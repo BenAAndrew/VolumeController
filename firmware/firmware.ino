00c7171b67af29aac830201635c02177266dfcba
BenAAndrew <benandrew89@gmail.com>
2023-08-24 18:14:02 +0200

    Fix display colour bleed

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -40,6 +40,7 @@ bool isConnected = false;
 
 void setup() {
   // Screen setup
+  tft.initR(INITR_REDTAB);
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(ST7735_BLACK);
   tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
