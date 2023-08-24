14236471704b8067f00faebdea29bf053b3895ce
BenAAndrew <benandrew89@gmail.com>
2023-08-24 18:55:00 +0200

    Fix paths for local app

--- volume_controller/utils.py
+++ volume_controller/utils.py
@@ -3,5 +3,5 @@ import sys
 
 
 def resource(relative_path):
-    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_path, relative_path)
+    base_path = getattr(sys, "_MEIPASS", None)
+    return os.path.join(base_path, relative_path) if base_path else relative_path
