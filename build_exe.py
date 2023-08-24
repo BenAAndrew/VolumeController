75ff92ee4c6c98ed5498e6345c60da116e111b58
BenAAndrew <benandrew89@gmail.com>
2023-08-24 18:56:32 +0200

    Hide console when running executable

--- build_exe.py
+++ build_exe.py
@@ -9,6 +9,13 @@ if __name__ == "__main__":
     icon_path = os.path.join("assets", "icon.ico")
     assert os.path.isfile(icon_path)
     PyInstaller.__main__.run(
-        ["main.py", "--onefile", "--clean", f"--icon={icon_path}", "--add-data=assets/;assets/",]
+        [
+            "main.py",
+            "--onefile",
+            "--noconsole",
+            "--clean",
+            f"--icon={icon_path}",
+            "--add-data=assets/;assets/",
+        ]
     )
     shutil.rmtree("build")
